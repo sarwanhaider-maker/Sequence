23487ab240da247d03280785478de4786aa6bf38
User <user@example.com>
2026-06-16 00:39:06 +0500

    Force Atlas database connection to bypass local MONGO_URL environment variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 7000,
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://sarwanhaider_db_user:DJQQSXyDziXvjB8x@cluster0.5jrgpai.mongodb.net/sequence?retryWrites=true&w=majority&appName=Cluster0",
+    MONGO_URL: "mongodb+srv://sarwanhaider_db_user:DJQQSXyDziXvjB8x@cluster0.5jrgpai.mongodb.net/sequence?retryWrites=true&w=majority&appName=Cluster0",
     corsOptions: {
         origin: '*', 
         methods: ["GET", "POST", "OPTIONS"]
